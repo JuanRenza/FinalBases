c3936876f84b87154d78a946a0f5e656372888fe
camilo520 <37637086+camilo520@users.noreply.github.com>
2020-05-25 18:16:57 -0500

    vista administrador producto

--- src/vista/Eliminar/EliminarProductos.java
+++ src/vista/Eliminar/EliminarProductos.java
@@ -89,7 +89,7 @@ public class EliminarProductos extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        String idProducto =jTextField1.getText();
+        int idProducto =Integer.parseInt(jTextField1.getText());
         ControlProducto cp =new ControlProducto();
         boolean t = cp.eliminarTipoProducto(idProducto);
 
